afae5bf417702f7487c03507d600d092c658cbda
daydreamer95 <38958256+daydreamer95@users.noreply.github.com>
2021-03-16 17:37:12 +0700

    Commit finishing form submit
    Problem: Handle first time init form data., set default data of selectbox

--- ReactNative_Demo/Components/AbsenceFormComponent.js
+++ ReactNative_Demo/Components/AbsenceFormComponent.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactNative, {
   TextInput,
   Text,
   View,
   StyleSheet,
-  Input,
+  Button,
+  ScrollView,
+  Alert
 } from "react-native";
 import {Picker} from '@react-native-picker/picker'
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -14,19 +16,116 @@ export default function AbsenceFormComponent() {
   const [emailAdress, setEmailAdress] = useState("");
   const [projectName, setProjectName] = useState("");
   const [absenceType, setAbsenceType] = useState("");
+  //FromDate state value
   const [fromDate, setFromDate] = useState(new Date());
+  const [showFromDatePicker, setshowFromDatePicker] = useState(false);
+  const [modeFromDatePicker, setModeFromDatePicker] = useState('');
+  //ToDate state value
   const [toDate, setToDate] = useState(new Date());
-  const [totalOffDate, setTotalOffDate] = useState(0);
+  const [showToDatePicker, setShowToDatePicker] = useState(false);
+  const [modeToDatePicker, setModeToDatePicker] = useState('');
+  //
+  const [totalOffDate, setTotalOffDate] = useState("");
   const [includedSaturday, setIncludedSaturday] = useState(0);
-  const [show, setShow] = useState(false);
 
-  // function handleChange(event) {
-  //   console.log(event);
-  //   setEmailAdress((previous) => "event.target.value");
-  // }
+  const formUrl =
+    "https://docs.google.com/forms/u/2/d/e/1FAIpQLSduUaiV98kwYhBbHmvuhg5uF3-g2DJG3sYv6p9gSbssJtq6GA/formResponse";
+
+  const handleSetTotalOffDate = (text) => {
+    let newText = '';
+    let numbers = '0123456789';
+
+    for (var i=0; i < text.length; i++) {
+        if(numbers.indexOf(text[i]) > -1 ) {
+            newText = newText + text[i];
+        }
+        else {
+            // your call back function
+            //alert("please enter numbers only");
+        }
+    }
+    setTotalOffDate(newText);
+  }
+
+  //From date picker
+  function onClickFromDateButton(){
+    setModeFromDatePicker('date');
+    setshowFromDatePicker(true);
+  }
+
+  function onClickFromTimeButton(){
+    setModeFromDatePicker('time');
+    setshowFromDatePicker(true);
+  }
+
+  function onChangeFromDate(event, date){
+    setFromDate(date); 
+    setshowFromDatePicker(false);
+  }
+  //To date picker
+  function onClickToDateButton(){
+    setModeToDatePicker('date');
+    setShowToDatePicker(true);
+  }
+
+  function onClickToTimeButton(){
+    setModeToDatePicker('time');
+    setShowToDatePicker(true);
+  }
+
+  function onChangeToDate(event, date){
+    setToDate(date); 
+    setShowToDatePicker(false);
+  }
+
+  handleSubmitForm =() => {
+    var formBody = createFormBody();
+
+    fetch(formUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+      body: formBody,
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log("Fail, error:", error);
+      });
+  }
+
+  function createFormBody(){
+    var data = {
+      "entry.631548922": projectName,
+      "entry.1000001_minute": fromDate.getMinutes(),
+      "entry.1000001_hour": fromDate.getHours(),
+      "entry.1000001_month": fromDate.getMonth(),
+      "entry.1000001_year": fromDate.getFullYear(),
+      "entry.1000001_day": fromDate.getDay(),
+      "entry.744351323_hour": toDate.getHours(),
+      "entry.744351323_minute": toDate.getMinutes(),
+      "entry.744351323_year": toDate.getFullYear(),
+      "entry.744351323_month": toDate.getMonth(),
+      "entry.744351323_day": toDate.getDay(),
+      "entry.572387094": totalOffDate,
+      "entry.1000000": absenceType,
+      "entry.1785497431": includedSaturday,
+      emailAddress: emailAdress
+    }
+    var formBody = [];
+    for (var property in data) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(data[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+    return formBody;
+  }
 
   return (
-    <View>
+    <ScrollView>
       <Text style={styles.textTitle}>Your email:</Text>
       <TextInput
         style={styles.textInput}
@@ -34,6 +133,7 @@ export default function AbsenceFormComponent() {
         onChangeText={(text) => setEmailAdress(text)}
         value={emailAdress}
       />
+      
       <Text style={styles.textTitle}>Project Name:</Text>
       <Picker style={styles.selectBox}
         selectedValue={projectName}
@@ -56,27 +156,101 @@ export default function AbsenceFormComponent() {
         <Picker.Item label="A1 / A2 / A3" value="A1 / A2 / A3"></Picker.Item>
         <Picker.Item label="Huỷ phép [Cancel the absence request]" value="Huỷ phép [Cancel the absence request]"></Picker.Item>
       </Picker>
-        
-      <Text style={styles.textTitle}>From Date:</Text>
 
-      <View>
-      <Text style={styles.textInput}
-            value={fromDate}
-            onFocus={()=>setShow(true)}>
-      </Text>
-      {show && <DateTimePicker
-          style ={styles.dateTimePicker}
-          testID="dateTimePicker"
-          value={fromDate}
-          mode="datetime"
-          is24Hour={true}
-          display="default"
-          onChange={(event,seletecDate) => setFromDate(seletecDate)}
-        />}
+      <Text style={styles.textTitle}>From Date:</Text>
+      <View style={styles.dateTimeContainer}>
+        <View style={styles.dateContainer}>
+          <Button style={styles.textInput}
+                title={fromDate.toLocaleDateString()}
+                onPress={()=>onClickFromDateButton()}>
+          </Button>
+        </View>
+        <View style={styles.timeContainer}>
+          <Button style={styles.textInput}
+                  title={fromDate.toLocaleTimeString()}
+                  onPress={()=>onClickFromTimeButton()}>
+          </Button>
+        </View>
       </View>
-    </View>
+      {showFromDatePicker && <DateTimePicker
+              style ={styles.dateTimePicker}
+              testID="fromDateTimePicker"
+              value={fromDate}
+              mode={modeFromDatePicker}
+              is24Hour={true}
+              display="default"
+              onChange={(event,date) => onChangeFromDate(event,date)}
+            />}
+
+      <Text style={styles.textTitle}>To Date:</Text>
+      <View style={styles.dateTimeContainer}>
+        <View style={styles.dateContainer}>
+          <Button style={styles.textInput}
+                title={toDate.toLocaleDateString()}
+                onPress={()=>onClickToDateButton()}>
+          </Button>
+        </View>
+        <View style={styles.timeContainer}>
+          <Button style={styles.textInput}
+                  title={toDate.toLocaleTimeString()}
+                  onPress={()=>onClickToTimeButton()}>
+          </Button>
+        </View>
+      </View>
+      {showToDatePicker && <DateTimePicker
+              style ={styles.dateTimePicker}
+              testID="toDateTimePicker"
+              value={toDate}
+              mode={modeToDatePicker}
+              is24Hour={true}
+              display="default"
+              onChange={(event,date) => onChangeToDate(event,date)}
+            />}
+      
+      <Text style={styles.textTitle}>Tổng số ngày nghỉ [Total number of leave day]:</Text>
+      <TextInput
+        style={styles.textInput}
+        placeHolder="Tổng số ngày nghỉ [Total number of leave day]:"
+        onChangeText={(text) => handleSetTotalOffDate(text)}
+        keyboardType='numeric'
+        value={totalOffDate}
+      />
+
+      <Text style={styles.textTitle}>Tính cả ngày thứ 7:</Text>
+      <Picker style={styles.selectBox}
+        selectedValue={includedSaturday.toString()}
+        onValueChange={(itemValue, itemIndex) => setIncludedSaturday(itemValue)}>
+        <Picker.Item label="0" value="0"></Picker.Item>
+        <Picker.Item label="1" value="1"></Picker.Item>
+        <Picker.Item label="2" value="2"></Picker.Item>
+      </Picker>
+
+      <Button style={styles.textInput}
+                title="Submit"
+                onPress={()=>Alert.alert(confirmAlert.title,confirmAlert.message,confirmAlert.options,confirmAlert.cancelable)}>
+          </Button>
+    </ScrollView>
   );
 }
+
+const confirmAlert = {
+  title: "CONFIRM",
+  message: "Are you confirm to submit",
+  options: [
+    {       
+      text: 'Cancel',       
+      //onPress: () => console.log('Cancel Pressed'),       
+      style: 'cancel',     
+    },     
+    {
+      text: 'OK', 
+      onPress: () => handleSubmitForm()
+    },   
+  ],
+  cancelable: {cancelable: false}
+}
+
+
 
 const styles = StyleSheet.create({
   textInput: {
@@ -106,5 +280,16 @@ const styles = StyleSheet.create({
     height: 40,
     borderColor: "#000000",
     borderWidth: 1,
+  },
+  dateTimeContainer: {
+    flexDirection: "row"
+  },
+  dateContainer: {
+    flex:2,
+    padding: 5
+  },
+  timeContainer: {
+    flex:2,
+    padding: 5
   }
 });
